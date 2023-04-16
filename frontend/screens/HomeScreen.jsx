2da2d1fe874da46533a201f84169eb04ad7f7351
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-04-16 00:48:50 -0500

    save token

--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -1,12 +1,18 @@
-import { Button, View, Text } from 'react-native'
-import React from 'react'
+import { Button, View, Text } from 'react-native';
+import React from 'react';
+import * as SecureStore from 'expo-secure-store';
 
 const HomeScreen = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-     )
-   }
+  const item = async () => {
+    const res = await SecureStore.getItemAsync('token');
+    if (res) console.log(res);
+  }
+  item();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
 
 export default HomeScreen
--- frontend/screens/SignInScreen.jsx
+++ frontend/screens/SignInScreen.jsx
@@ -1,21 +1,64 @@
-import { Button, View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react';
+import { Button, View, Text, StyleSheet, Image, Alert } from 'react-native'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { isEmpty } from 'lodash';
+import * as SecureStore from 'expo-secure-store';
 import { TextInput } from 'react-native-gesture-handler';
+import { LOCAL_IP } from '../constants/constants.js';
 
 const SignInScreen = ({ navigation }) => {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (key, value) => {
+        setData(prev => ({ ...prev, [key]: value}));
+    }
+    
+    const checkEmpty = () => {
+        return isEmpty(data.email) || isEmpty(data.password);
+    }
+
+    const saveToken = async (token) => {
+        await SecureStore.setItemAsync('token', token);
+    }
+
+    const handleSubmit = async () => {
+        if (checkEmpty()) {
+            Alert.alert('All fields are required');
+            return;
+        }
+        axios({
+            url: `http://${LOCAL_IP}:3005/login`,
+            method: "POST",
+            data,
+            headers: { "Access-Control-Allow-Origin": "*"}
+        }).then((res) => {
+            console.log(res.data);
+            saveToken(res.data);
+            navigation.navigate("Main");
+        }).catch((err) => {
+            Alert.alert(err.message);
+        })
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.brandname}>ViFri</Text>
             <TextInput style={styles.textinput}
-                placeholder="Username"
+                placeholder="Email"
+                onChangeText={(e) => handleChange('email', e)}
             />
             <TextInput style={styles.textinput}
                 placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={(e) => handleChange('password', e)}
             />
             <Button
                 title="Sign In"
-                onPress={() => navigation.navigate("Main")}
-                />
+                onPress={handleSubmit}
+            />
             <Button
                 title="Create Account?"
                 onPress={() => navigation.navigate("SignUp")}

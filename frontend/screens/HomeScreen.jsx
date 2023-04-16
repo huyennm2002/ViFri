bb4535a2f57c9821d7f3711e464e18edd6676cfc
jacekeagy <jacekeagy22@gmail.com>
2023-04-15 23:26:01 -0500

    Started on home screen

--- frontend/screens/HomeScreen.jsx
+++ frontend/screens/HomeScreen.jsx
@@ -1,12 +1,47 @@
-import { Button, View, Text } from 'react-native'
+import { ScrollView, View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const HomeScreen = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-     )
-   }
+  return (
+      <ScrollView style={styles.container}>
+      <Text style={styles.hometitle}>Welcome Back!</Text>
+      <Text style={styles.itemcounttitle}>Number of Items in Fridge: ???</Text>
+      <Text style={styles.itemcounttitle}>Number of items about to expire: ???</Text>
+    </ScrollView>
+   )
+ }
 
-export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 1,
+    backgroundColor: 'lightsteelblue',
+  },
+  hometitle: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    marginTop: 50,
+    textAlign: 'center',
+  },
+  itemcounttitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    marginRight: 25,
+    marginLeft: 25,
+    marginTop: 15,
+  },
+  expirecounttitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    marginRight: 25,
+    marginLeft: 25,
+    marginTop: 15
+  },
+  })
+  
+  export default HomeScreen
